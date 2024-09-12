b746b78eeb68d19ed359f6766bde36587d5cc1de
murasakiaura <isabella002211@gmail.com>
2024-09-12 20:38:35 +0100

    Back-End Setup: Initial Folder Structure & Git Repo

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require('cors');
+//const commentsRouter = require('./routes/comments.js');
+//const usersRouter = require('./routes/users.js');
+const app = express()
+const port = 3000
+
+// Middleware
+app.use(express.json()) 
+app.use(cors()); 
+app.use(express.static('public'))
+app.use((req, res, next) => {
+  console.log("Welcome to InStock!");
+ 
+  next();
+})
+
+app.use((req, res, next) => {
+  console.log("Welcome to InStock!!!");
+  next();
+})
+
+// CORS middleware
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+//app.use('/comments', commentsRouter);
+//app.use('/users', usersRouter);
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
