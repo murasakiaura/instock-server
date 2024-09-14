4f4f49e4494d310c69e3ea9a7fe89a88e2d8f389
davidmaxy1994 <maksymiukdavid@gmail.com>
2024-09-14 11:24:49 +0100

    Implemented SPF2-5: Set up Knex and database connection for instockDB

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
-const express = require('express')
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 //const commentsRouter = require('./routes/comments.js');
 //const usersRouter = require('./routes/users.js');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 // Middleware
-app.use(express.json()) 
-app.use(cors()); 
-app.use(express.static('public'))
+app.use(express.json());
+app.use(cors());
+app.use(express.static("public"));
 app.use((req, res, next) => {
   console.log("Welcome to InStock!");
- 
+
   next();
-})
+});
 
 app.use((req, res, next) => {
   console.log("Welcome to InStock!!!");
   next();
-})
+});
 
 // CORS middleware
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/", (req, res) => {
+  res.send("HEEEEEEEELLLLO");
 });
 
 //app.use('/comments', commentsRouter);
 //app.use('/users', usersRouter);
 
-
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
