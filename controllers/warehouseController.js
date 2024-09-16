8f28ad1e025fffc83ebb34acf58ef4c17e94d587
davidmaxy1994 <maksymiukdavid@gmail.com>
2024-09-16 21:50:56 +0100

    Implement feature/SPF2-18: Warehouse Edit API
    Implemented the backend API for editing warehouse details.
    
    Changes made:
    
    - Created the `updateWarehouseDetails` controller in `warehouseController.js` to handle PUT requests.
    
    - Added data validation using Joi to ensure non-empty values and proper format.
    
    - Integrated the new routes in `warehouseRoutes.js`.

---
+++ controllers/warehouseController.js
@@ -0,0 +1,58 @@
+const Joi = require("joi");
+const knex = require("knex")(require("../knexfile"));
+
+const updateWarehouseDetails = async (req, res) => {
+  try {
+    const warehouseId = parseInt(req.params.id);
+
+    if (!warehouseId || isNaN(warehouseId)) {
+      return res.status(400).json({ message: "Invalid warehouse ID." });
+    }
+
+    const warehouseSchema = Joi.object({
+      warehouse_name: Joi.string().required(),
+      address: Joi.string().required(),
+      city: Joi.string().required(),
+      country: Joi.string().required(),
+      contact_name: Joi.string().required(),
+      contact_position: Joi.string().required(),
+      contact_phone: Joi.string()
+        .pattern(/^\+?[0-9\s-().]*$/)
+        .required(),
+      contact_email: Joi.string().email().required(),
+    });
+
+    const { error } = warehouseSchema.validate(req.body, { abortEarly: false });
+    if (error) {
+      return res.status(400).json({
+        message: error.details.map((detail) => detail.message).join(", "),
+      });
+    }
+
+    const warehouseExists = await knex("warehouses")
+      .where("id", warehouseId)
+      .first();
+    if (!warehouseExists) {
+      return res.status(404).json({ message: "Warehouse not found." });
+    }
+
+    await knex("warehouses").where("id", warehouseId).update(req.body);
+
+    const updatedWarehouse = await knex("warehouses")
+      .where("id", warehouseId)
+      .first();
+
+    res.status(200).json({
+      message: "Warehouse updated successfully",
+      data: updatedWarehouse,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: `Error updating warehouse: ${error.message}` });
+  }
+};
+
+module.exports = {
+  updateWarehouseDetails,
+};
