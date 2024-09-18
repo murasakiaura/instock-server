f232e3669cdef99b5f2c353d5840bbf6bbf387f3
Russ07 <90725405+Russ07@users.noreply.github.com>
2024-09-18 22:25:41 +0100

    added a check in case warehouses list is empty

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -64,6 +64,9 @@ const updateWarehouseDetails = async (req, res) => {
 const getAllWarehouses = async(req, res) => {
   try {
     const allWarehouses = await knex("warehouses");
+    if (allWarehouses.length === 0) {
+      return res.status(200).json({ message: 'No warehouses found' });
+    }
     res.status(200).json(allWarehouses);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch warehouses' });
