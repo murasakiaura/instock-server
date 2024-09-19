cab4a13d33a46480e627f72f0650eeec0d4f146c
Russ07 <90725405+Russ07@users.noreply.github.com>
2024-09-19 23:34:34 +0100

    GET Inventories for a Given Warehouse

--- routes/inventoriesRoutes.js
+++ routes/inventoriesRoutes.js
@@ -5,12 +5,13 @@ const {
   createNewInventory,
   getAllInventoryItems,
   updateInventoryByWarehouseId,
+  getWarehouseInventories,
 } = require("../controllers/inventoriesControllers");
 
 router.post("/", createNewInventory);
 router.put("/:inventory_id", updateInventoryByWarehouseId);
 router.get('/', getAllInventoryItems);
-
+router.get('/:warehouse_id', getWarehouseInventories);
 
 
 module.exports = router;
