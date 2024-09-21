7ecba6691506eaa0efac0ecd514704692daca855
murasakiaura <isabella002211@gmail.com>
2024-09-21 12:24:01 +0100

    updates

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-const express = require('express')
-const cors = require('cors');
-const inventoriesRoutes = require('./routes/inventoriesRoutes');
-const app = express()
-const port = 3000
-=======
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const inventoriesRoutes = require("./routes/inventoriesRoutes");
 const warehouseRoutes = require("./routes/warehouseRoutes");
 const port = process.env.PORT || 3000;
->>>>>>>>> Temporary merge branch 2
 
 // Middleware
 app.use(express.json());
@@ -22,14 +14,9 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-<<<<<<< HEAD
-// Routes 
-app.use('/inventories/:warehouse_id', inventoriesRoutes);
-=======
 // Routes
 app.use("/api/inventories", inventoriesRoutes);
 app.use("/api/warehouses", warehouseRoutes);
->>>>>>> 23222c399f2863fefddda719c897a996ae1d976d
 
 
 app.listen(port, () => {
